1ada1ad167d77fa24dd3feaf63e39d5465b6414e
Atid Puwatnuttasit <atid.puw@gmail.com>
2018-06-15 23:19:24 +0700

    Update Text Component.

---
+++ Unity-ScriptableObjects-Text101/Assets/AdventureGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+	[SerializeField] private Text _textComponent;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_textComponent.text = "I'm added programmatically";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
