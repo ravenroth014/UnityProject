4362afd4b3b97d93143ee0af4088f2e6fc35e82a
Atid Puwatnuttasit <atid.puw@gmail.com>
2018-06-21 16:19:41 +0700

    Test using scriptable object.

--- Unity-ScriptableObjects-Text101/Assets/Scripts/AdventureGame.cs
+++ Unity-ScriptableObjects-Text101/Assets/Scripts/AdventureGame.cs
@@ -4,11 +4,15 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
 	[SerializeField] private Text _textComponent;
+	[SerializeField] private State _startingState;
+
+	private State _state;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		_textComponent.text = "I'm added programmatically";
+		_state = _startingState;
+		_textComponent.text = _state.GetStateStory();
 	}
 	
 	// Update is called once per frame
--- Unity-ScriptableObjects-Text101/Assets/Scripts/State.cs
+++ Unity-ScriptableObjects-Text101/Assets/Scripts/State.cs
@@ -3,7 +3,12 @@
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-  // first parameter is limited line
-  // second parameter is number of lines before scrollable
-  [TextArea (14,10)] [SerializeField] private string stroyText;
+    // first parameter is limited line
+    // second parameter is number of lines before scrollable
+    [TextArea (14,10)] [SerializeField] private string _stroyText;
+
+    public string GetStateStory()
+    {
+        return _stroyText;
+    }
 }
