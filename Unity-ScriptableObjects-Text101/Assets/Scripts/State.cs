68b1477d5cd25da661cb46a90d77808c3291dc51
Atid Puwatnuttasit <atid.puw@gmail.com>
2018-06-21 15:52:28 +0700

    Introduce Scriptable Object.

---
+++ Unity-ScriptableObjects-Text101/Assets/Scripts/State.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+  // first parameter is limited line
+  // second parameter is number of lines before scrollable
+  [TextArea (14,10)] [SerializeField] private string stroyText;
+}
